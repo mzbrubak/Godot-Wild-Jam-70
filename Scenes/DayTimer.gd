a4dea77e12f3637eb66e918362921923a0718bb7
Morgan  Brubaker <43830512+mzbrubak@users.noreply.github.com>
2024-06-19 21:51:53 -0700

    Setup for NPC Navigation
    mostly just committing now to not worry about GItHub desyncing if there are more commits overnight.  Seems like I'll have to learn how to effectively use a 2DNavigationServer to tell NPCs how to get to where they need to go.  I've done some initial work on the navigation agents at least

--- Scenes/DayTimer.gd
+++ Scenes/DayTimer.gd
@@ -5,17 +5,17 @@ var daylength=100
 var TimeUI
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	timeint=5000
+	timeint=0
 	TimeUI=find_child("Time")
 	TimeUI.text=str(timeint)
-	start()
+	
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	pass
 
 func onTimeout():
-	print("Testing")
+	#print("Testing")
 	timeint+=1
 	TimeUI.text=str(timeint)
 	AnnounceTime.emit(timeint)
--- Scenes/Possessable_NPC.gd
+++ Scenes/Possessable_NPC.gd
@@ -5,9 +5,15 @@ var patience:int = 3 #how long character will try to do tasks that are late
 var pathfinder
 var isPlayer:bool=false #indicates possession status
 var interactionArea
+var testTask: Task
+var navigationReady=false
 func _ready():
 	super._ready()
 	interactionArea=find_child("InteractionArea")
+	pathfinder=find_child("NavigationAgent2D")
+	schedule.currenttask=Task.new(20, Vector2(360,300), 0,[])
+	pathfinder.set_target_position(schedule.currenttask.location)
+	call_deferred("NavigationSetup")
 
 func _process(delta):
 	if isPlayer:
@@ -29,11 +35,20 @@ func _physics_process(delta):
 		if velocity.length() > 0:
 			velocity = velocity.normalized() * speed
 		move_and_slide()
+	elif navigationReady and !pathfinder.is_navigation_finished():
+		var next_path_position=pathfinder.get_next_path_position()
+		velocity=global_position.direction_to(next_path_position)*speed
+		print(global_position.direction_to(next_path_position))
+		move_and_slide()
 
 
 func do_currenttask():
 	pass#actually do a thing
+	getnexttask()
+
+func getnexttask():
 	schedule.popTask()
+	pathfinder.set_target_position(schedule.currenttask)
 
 func IfBodyEntered(Body):
 	print(Body," entered interaction area of ",self)
@@ -50,8 +65,18 @@ func becomePossessed():
 	isPlayer=true
 	interactionArea.get_child(0).disabled=true
 	set_collision_layer_value(3, true)
+	
 func endPossession():
 	isPlayer=false
 	interactionArea.get_child(0).disabled=false
 	set_collision_layer_value(3,false)
 	possessionEnding.emit(self)
+
+func startNavigation(time:int):
+#the pathfinding algorithm only works if there is enough lead time to make a navigation map
+#in an attempt to allow for this in an easier way, I'm just going to wait for a certain time to start
+	var triggertime=5
+	if time>=triggertime:
+		navigationReady=true
+		pathfinder.set_max_speed(speed)
+		
